2d371a1320f728bc76781af49897fea5edc0e2bc
Shrawan <shrawankumarshrestha@gmail.com>
2023-01-22 16:21:42 +0545

    completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,99 +1,47 @@
 $(function () {
-  // $(".scroll-down").click(function () {
-  //   $("html, body").animate(
-  //     { scrollTop: $(".section--about").offset().top },
-  //     "fast"
-  //   );
-  //   return false;
-  // });
-
-  var lastScroll = 0;
-  var header = $(".navbar").outerHeight();
-  $(window).scroll(function () {
-    if ($(window).scrollTop() >= header) {
-      $("#header").addClass("sticky");
-      var head = $(".navbar").outerHeight();
-      document.documentElement.style.setProperty("--headerWidth", head + "px");
-    } else {
-      $("#header").removeClass("sticky");
-      document.documentElement.style.setProperty(
-        "--headerWidth",
-        header + "px"
-      );
-    }
-  });
-
-  // change is-checked class on buttons
-  $(".button-group").each(function (i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on("click", "button", function () {
-      $buttonGroup.find(".is-checked").removeClass("is-checked");
-      $(this).addClass("is-checked");
-    });
-  });
-
-  // init Isotope
-  var $grid = $(".products-group--isotope").imagesLoaded(function () {
-    $grid.isotope({
-      itemSelector: ".filter-item",
-      layoutMode: "fitRows",
-      fitRows: {
-        // gutter: 16,
-      },
-      // masonry: {
-      //   columnWidth: 158,
-      // },
-      filter: ".flowers",
-    });
-    $(".products-group--isotope").css({ opacity: 1 });
-  });
-  // bind filter button click
-  $(".filters-button-group").on("click", "button", function () {
-    var filterValue = $(this).attr("data-filter");
-    // use filterFn if matches value
-    // filterValue = filterFns[ filterValue ] || filterValue;
-    $grid.isotope({ filter: filterValue });
-  });
-
-  $(".js-testimonials").slick({
-    dots: true,
-    slidesToShow: 1,
-    arrows: false,
-    adaptiveHeight: true,
-    autoplay: true,
-  });
-
-  function calccontactHeight() {
-    if (".contact-info".length > 0) {
-      var contactHeight = $(".contact-info").outerHeight();
-      $("body").css("--contactHeight", contactHeight + "px");
-      console.log(contactHeight);
-    }
-  }
-  calccontactHeight();
-
-  $(window).on("resize", function () {
-    calccontactHeight();
-  });
+  AOS.init();
 
   $(".js-counter").countUp({
     time: 2000,
     delay: 10,
   });
+
+  $("#hero-carousel").slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: false,
+    variableWidth: false,
+    autoplay: false,
+    adaptiveHeight: false,
+    speed: 900,
+    autoplaySpeed: 700,
+    appendArrows: $("#slidecontrols"),
+    prevArrow: `<div class="icon">
+      <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16 20L10 14.4553L16 9" stroke="#0093FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <circle cx="14" cy="14" r="13.5" stroke="#0093FF"/>
+      </svg>
+    </div>
+    
+    `,
+    nextArrow: `<div class="icon">
+      <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 20L18 14.4553L12 9" stroke="#0093FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <circle r="13.5" transform="matrix(-1 0 0 1 14 14)" stroke="#0093FF"/>
+      </svg>
+    </div>
+    
+    `,
+  });
+
+  AOS.refresh();
 });
 
-(function () {
-  "use strict";
-  AOS.init();
-  // document
-  //   .querySelector("#navbarSideCollapse")
-  //   .addEventListener("click", function () {
-  //     // alert('hello');
-  //     document.querySelector(".offcanvas-collapse").classList.toggle("open");
-  //   });
-
-  document.querySelector("#searchbtn").addEventListener("click", function () {
-    // alert('hello');
-    document.querySelector("#searchbox").classList.toggle("open");
-  });
-})();
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
